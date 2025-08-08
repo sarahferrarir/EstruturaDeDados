ca50553b8d328b385114e98516f6cdd4f47fab51
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-08 10:38:42 -0300

    Add files via upload

---
+++ BeecrowdExercises/beecrowd1005.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    float a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    printf("diferenca + %;01f\n", ((a * b) - (c * d)));
+}
