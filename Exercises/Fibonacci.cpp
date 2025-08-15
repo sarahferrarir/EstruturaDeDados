6322b9d7832707b9779ea9b8b918693be4b01791
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-15 09:18:38 -0300

    Create Fibonacci.cpp

---
+++ Exercises/Fibonacci.cpp
@@ -0,0 +1,43 @@
+// Tarefa: Desenvolva uma função Fibonacci recursiva
+
+// Minha resposta
+#include <iostream>
+using namespace std;
+
+int fibo(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Posição n: ";
+    cin >> n;
+
+    cout << "Fibonacci de " << n << " é " << fibo(n) << endl;
+
+    return 0;
+}
+
+
+// Gabarito do Professor 
+#include <iostream>
+using namespace std;
+
+long long int fibo(int x){
+    if(x == 0)
+        return 0;
+    if(x == 1)
+        return 1;
+    return fibo(x - 1) + fibo(x -2);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fibo(n) << endl;
+
+    return 0;
+}
