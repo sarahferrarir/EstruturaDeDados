8b106944fe8c29614c69e7d2ad47c5be720c6de2
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-15 11:35:00 -0300

    Create beecrowd1001.cpp

---
+++ BeecrowdExercises/lista1/beecrowd1001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    float a, b;
+    cin >> a;
+    cin >> b;
+    float x = a + b;
+
+    cout << "X = " << x << endl;
+
+    return 0;
+}
