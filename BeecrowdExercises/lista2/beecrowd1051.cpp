9ad2269c16ffc4f3407a243eb3088c50b99324c8
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-15 11:52:18 -0300

    Create beecrowd1051.cpp

---
+++ BeecrowdExercises/lista2/beecrowd1051.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N <= 0) return 0;
+
+    int ant2 = 0; 
+    int ant1 = 1;
+
+    for (int i = 0; i < N; i++) {
+        if (i == 0) {
+            cout << ant2; 
+        } else if (i == 1) {
+            cout << " " << ant1; 
+        } else {
+            int atual = ant1 + ant2; 
+            cout << " " << atual;
+            ant2 = ant1;
+            ant1 = atual;
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
