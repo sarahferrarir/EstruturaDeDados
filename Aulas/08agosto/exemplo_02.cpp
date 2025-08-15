b211148da58fc1a3e9bdff1e414a064646216f09
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-15 09:31:02 -0300

    Add files via upload

---
+++ Aulas/08agosto/exemplo_02.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Libs Entrada/Saída C++
+#include <cstdio>  // // Libs Entrada/Saída C
+using namespace std;
+
+// Tipo nome (conjunto de parametros){} Assinatura da função
+int main(){
+    int x = 10;
+    cout << "Valor de x é:" << endl;
+    cout << x << endl;
+
+    cout << "Valor de x é:" << x << endl;
+
+    return 0;
+
+}
+
---
+++ Aulas/08agosto/exemplo_03.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Libs Entrada/Saída C++
+#include <cstdio>  // // Libs Entrada/Saída C
+using namespace std;
+
+// Tipo nome (conjunto de parametros){} Assinatura da função
+int main(){
+    int x,y;
+    cin >> x;
+    cin >> y; 
+    cout << "X = " << x+y << endl;
+    return 0;
+
+}
+
