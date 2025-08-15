a4e2546aec5eaf5325911254c2acbf040e1d91c9
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-15 12:12:21 -0300

    Create beecrowd1153.cpp

---
+++ BeecrowdExercises/lista2/beecrowd1153.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int res = 1;
+      for (int i = 1; i <= n; ++i) {
+         res *= i;
+    }
+    cout << res << endl;
+
+    return 0;
+}
