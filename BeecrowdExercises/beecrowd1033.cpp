bdb8d91a447ee5fc2b26457276488864ec6cc2cc
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-15 11:29:35 -0300

    Add files via upload

---
+++ BeecrowdExercises/beecrowd1033.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    int b;
+    int caseNum = 1;
+
+    while (cin >> n >> b) {
+        if (n == 0 && b == 0) break;
+
+        if (n == 0) {
+            cout << "Case " << caseNum++ << ": " << n << " " << b << " 1" << endl;
+            continue;
+        }
+        if (n == 1) {
+            cout << "Case " << caseNum++ << ": " << n << " " << b << " 1" << endl;
+            continue;
+        }    
+
+        long long ini2 = 1 % b; 
+        long long ini1 = 1 % b; 
+        long long atual; 
+
+        for (long long i = 2; i <= n; i++) {
+            atual = (ini1 + ini2 + 1) % b; 
+            ini2 = ini1;
+            ini1 = atual;
+        }
+
+        cout << "Case " << caseNum++ << ": " << n << " " << b << " " << atual << endl;
+    }
+
+    return 0;
+}
