246f73975aa791d9e0edfb80517c57b88ea3a6aa
Sarah Ferrari <sarahferrarir@gmail.com>
2025-08-29 07:59:30 -0300

    Update beecrowd2846.cpp

--- BeecrowdExercises/lista2/beecrowd2846.cpp
+++ BeecrowdExercises/lista2/beecrowd2846.cpp
@@ -1,26 +1,26 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 int main() {
     long long K;
-    cin >> K; 
+    cin >> K;
     
     long long f1 = 1;
     long long f2 = 2;
-
+    
     while (true) {
-        long long f3 = f1 + f2;        
-        long long gap = f3 - f2 - 1;   
-
-        if (K > gap) {                 
-            K -= gap;                  
-            f1 = f2;                   
+        long long f3 = f1 + f2;
+        long long gap = f3 - f2 - 1;
+        
+        if (K > gap) {
+            K-= gap;
+            f1 = f2;
             f2 = f3;
         } else {
-            cout << (f2 + K) << endl; 
+            cout << (f2 + K) << endl;
             break;
         }
     }
-
+    
     return 0;
 }
