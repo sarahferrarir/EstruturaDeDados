421264cf957b6cf8648fdfc5cdd145b6fc0c815e
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-12 10:15:14 -0300

    Create beecrowd2702.cpp

---
+++ BeecrowdExercises/lista3/beecrowd2702.cpp
@@ -0,0 +1,21 @@
+// Título: Escolha Difícil
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Ca, Ba, Pa; 
+    int Cr, Br, Pr; 
+    cin >> Ca >> Ba >> Pa;
+    cin >> Cr >> Br >> Pr;
+
+    int insatisfeitos = 0; 
+
+    if (Cr > Ca) insatisfeitos += Cr - Ca;
+    if (Br > Ba) insatisfeitos += Br - Ba;
+    if (Pr > Pa) insatisfeitos += Pr - Pa;
+
+    cout << insatisfeitos << endl;
+
+    return 0;
+}
