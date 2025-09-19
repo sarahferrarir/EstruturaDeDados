3d1cdb2c97a3d51f3afcfcdc77c537057d99f023
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-19 08:49:54 -0300

    Update BubbleSort.cpp

--- Aulas/12setembro/BubbleSort.cpp
+++ Aulas/12setembro/BubbleSort.cpp
@@ -6,21 +6,28 @@
 using namespace std;
 
 int main() {
+    // Declaração e inicialização do vetor com 8 elementos
     int V[8] = {1, 23, 5, 10, 5, -1, -2, 0};
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 7; j++) {
-            if (V[j] > V[j+1]) { 
+
+    // Algoritmo Bubble Sort
+    // Dois loops aninhados: percorrem o vetor e realizam trocas
+    for (int i = 0; i < 8; i++) {          // Loop externo: controla quantas vezes o vetor será percorrido
+        for (int j = 0; j < 7; j++) {      // Loop interno: compara elementos adjacentes
+            if (V[j] > V[j+1]) {           // Se o elemento atual for maior que o próximo
+                // Troca os elementos de posição
                 int aux = V[j];
                 V[j] = V[j+1];
                 V[j+1] = aux;
             }
         }
     }
+
+    // Impressão do vetor já ordenado
     cout << "Vetor ordenado: ";
     for (int i = 0; i < 8; i++) {
-        cout << V[i] << " ";
+        cout << V[i] << " ";   // Exibe cada elemento do vetor
     }
     cout << endl;
 
-    return 0;
-} 
+    return 0;  
+}
